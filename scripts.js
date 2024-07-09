3d18c37422334af2bd15e89d10e21cef3182c75a
chusete289 <164486862+chusete289@users.noreply.github.com>
2024-07-09 11:39:02 +0200

    Agregar funcionalidad bilingüe y mejorar diseño

--- scripts.js
+++ scripts.js
@@ -39,3 +39,22 @@ function switchLanguage(lang) {
         document.querySelector("#contact p:nth-child(3)").innerText = "Phone: +34 616 460 352";
     }
 }
+const carouselSlide = document.querySelector('.carousel-slide');
+const images = document.querySelectorAll('.carousel img');
+
+let counter = 0;
+const size = images[0].clientWidth;
+
+document.querySelector('.next').addEventListener('click', () => {
+    if (counter >= images.length - 1) return;
+    carouselSlide.style.transition = "transform 0.5s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.5s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
