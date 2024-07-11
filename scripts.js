e4edf971ca6141056a55487494f9fce79433b97f
chusete289 <164486862+chusete289@users.noreply.github.com>
2024-07-11 16:44:33 +0200

    Agregar funcionalidad bilingüe y mejorar diseño

--- scripts.js
+++ scripts.js
@@ -3,42 +3,67 @@ function switchLanguage(lang) {
         document.documentElement.lang = "es";
         document.querySelector("#hero-description").innerText = "Desarrollador Web | Freelancer";
         document.querySelector("#about h2").innerText = "Sobre mí";
-        document.querySelector("#about-text").innerText = "Soy un apasionado programador informatico con altas capacidades como desarrollador web y una amplia experiencia en la creación, mantenimiento y diseño de sitios webs dinámicos y responsivos. He trabajado con diversas tecnologías y frameworks para ofrecer soluciones personalizadas y de alta calidad para mis clientes. Mi objetivo es utilizar mis habilidades para ayudar a las empresas a alcanzar principalmente sus metas a través de las mejores soluciones tecnológicas más innovadoras actualmente y que sigan creciendo y cumpliendo sus objetivos sin apenas darse cuenta.";
+        document.querySelector("#about-text").innerText = "Soy un apasionado desarrollador web con experiencia en crear, mantener y diseñar sitios web dinámicos y responsivos. Mi objetivo es transformar ideas en proyectos digitales atractivos, ayudando a las empresas a alcanzar sus metas con soluciones tecnológicas innovadoras.";
         document.querySelector("#skills h2").innerText = "Habilidades";
+        document.querySelector("#skills ul").innerHTML = `
+            <li>Desarrollo Frontend: HTML, CSS, JavaScript, React</li>
+            <li>Desarrollo Backend: Node.js, Express, Python, Django</li>
+            <li>Base de Datos: MongoDB, MySQL</li>
+            <li>Control de Versiones: Git, GitHub</li>`;
         document.querySelector("#projects h2").innerText = "Proyectos";
-        document.querySelector("#projects .project:nth-child(1) h3").innerText = "Desarrollo de una plataforma de E-commerce";
-        document.querySelector("#projects .project:nth-child(1) p").innerText = "Desarrollé una plataforma de comercio electrónico utilizando React y Node.js, proporcionando una experiencia de usuario optimizada y funciones avanzadas de administración de productos.";
+        document.querySelector("#projects .project:nth-child(1) h3").innerText = "E-commerce Platform: Culinary King";
+        document.querySelector("#projects .project:nth-child(1) p").innerText = "Desarrollé una plataforma de comercio electrónico con una colección de productos alimenticios, presentada con características de diseño modernas y CSS avanzado.";
         document.querySelector("#projects .project:nth-child(1) a").innerText = "Ver proyecto";
-        document.querySelector("#projects .project:nth-child(2) h3").innerText = "Aplicación de gestión de proyectos";
-        document.querySelector("#projects .project:nth-child(2) p").innerText = "Creé una aplicación de gestión de proyectos con Angular y Firebase, permitiendo la colaboración en tiempo real y la administración eficiente de tareas y recursos.";
+        document.querySelector("#projects .project:nth-child(2) h3").innerText = "Project Management Application: TravelLite";
+        document.querySelector("#projects .project:nth-child(2) p").innerText = "Creé una aplicación web para una agencia de viajes, utilizando Django y MySQL, con un diseño de Materialize CSS.";
         document.querySelector("#projects .project:nth-child(2) a").innerText = "Ver proyecto";
-        document.querySelector("#projects .project:nth-child(3) h3").innerText = "Herramienta de análisis de datos";
-        document.querySelector("#projects .project:nth-child(3) p").innerText = "Desarrollé una herramienta de análisis de datos utilizando Python y Django, facilitando la visualización y el análisis de grandes volúmenes de datos para la toma de decisiones estratégicas.";
+        document.querySelector("#projects .project:nth-child(3) h3").innerText = "Data Analysis Tool: Adventour";
+        document.querySelector("#projects .project:nth-child(3) p").innerText = "Desarrollé una herramienta de análisis de datos utilizando HTML, CSS y JavaScript, facilitando la visualización y el análisis de datos para la toma de decisiones estratégicas.";
         document.querySelector("#projects .project:nth-child(3) a").innerText = "Ver proyecto";
+        document.querySelector("#testimonials h2").innerText = "Testimonios";
+        document.querySelector("#testimonials .testimonial:nth-child(1) p:nth-child(2)").innerText = "Jesús es un desarrollador increíble. Su trabajo ha sido esencial para el éxito de nuestro proyecto.";
+        document.querySelector("#testimonials .testimonial:nth-child(1) p:nth-child(3)").innerText = "- María González, CEO de TechCorp";
+        document.querySelector("#testimonials .testimonial:nth-child(2) p:nth-child(2)").innerText = "La calidad y atención al detalle que Jesús ofrece es excepcional. Lo recomendaría sin dudar.";
+        document.querySelector("#testimonials .testimonial:nth-child(2) p:nth-child(3)").innerText = "- Juan Pérez, Gerente de Proyecto en Innovate Ltd.";
         document.querySelector("#contact h2").innerText = "Contacto";
-        document.querySelector("#contact p:nth-child(2)").innerText = "Email: jesuscarrilero@example.com";
-        document.querySelector("#contact p:nth-child(3)").innerText = "Teléfono: +34 616 460 352";
+        document.querySelector("#contact form label[for='name']").innerText = "Nombre:";
+        document.querySelector("#contact form label[for='email']").innerText = "Email:";
+        document.querySelector("#contact form label[for='message']").innerText = "Mensaje:";
+        document.querySelector("#contact form button").innerText = "Enviar";
     } else if (lang === 'en') {
         document.documentElement.lang = "en";
         document.querySelector("#hero-description").innerText = "Web Developer | Freelancer";
         document.querySelector("#about h2").innerText = "About Me";
-        document.querySelector("#about-text").innerText = "I am a passionate web developer with extensive experience in creating, maintaining, and designing dynamic and responsive websites. I have worked with various technologies and frameworks to deliver customized, high-quality solutions for my clients. My goal is to use my skills to help companies achieve their goals through innovative technological solutions.";
+        document.querySelector("#about-text").innerText = "I am a passionate web developer with extensive experience in creating, maintaining, and designing dynamic and responsive websites. My goal is to transform ideas into attractive digital projects, helping companies achieve their goals with innovative technological solutions.";
         document.querySelector("#skills h2").innerText = "Skills";
+        document.querySelector("#skills ul").innerHTML = `
+            <li>Frontend Development: HTML, CSS, JavaScript, React</li>
+            <li>Backend Development: Node.js, Express, Python, Django</li>
+            <li>Database: MongoDB, MySQL</li>
+            <li>Version Control: Git, GitHub</li>`;
         document.querySelector("#projects h2").innerText = "Projects";
-        document.querySelector("#projects .project:nth-child(1) h3").innerText = "E-commerce Platform Development";
-        document.querySelector("#projects .project:nth-child(1) p").innerText = "Developed an e-commerce platform using React and Node.js, providing an optimized user experience and advanced product management features.";
+        document.querySelector("#projects .project:nth-child(1) h3").innerText = "E-commerce Platform: Culinary King";
+        document.querySelector("#projects .project:nth-child(1) p").innerText = "Developed an e-commerce platform with a collection of food products, featuring modern design characteristics and advanced CSS.";
         document.querySelector("#projects .project:nth-child(1) a").innerText = "View Project";
-        document.querySelector("#projects .project:nth-child(2) h3").innerText = "Project Management Application";
-        document.querySelector("#projects .project:nth-child(2) p").innerText = "Created a project management application with Angular and Firebase, enabling real-time collaboration and efficient task and resource management.";
+        document.querySelector("#projects .project:nth-child(2) h3").innerText = "Project Management Application: TravelLite";
+        document.querySelector("#projects .project:nth-child(2) p").innerText = "Created a web application for a travel agency, using Django and MySQL, with a Materialize CSS design.";
         document.querySelector("#projects .project:nth-child(2) a").innerText = "View Project";
-        document.querySelector("#projects .project:nth-child(3) h3").innerText = "Data Analysis Tool";
-        document.querySelector("#projects .project:nth-child(3) p").innerText = "Developed a data analysis tool using Python and Django, facilitating the visualization and analysis of large data sets for strategic decision-making.";
+        document.querySelector("#projects .project:nth-child(3) h3").innerText = "Data Analysis Tool: Adventour";
+        document.querySelector("#projects .project:nth-child(3) p").innerText = "Developed a data analysis tool using HTML, CSS, and JavaScript, facilitating the visualization and analysis of data for strategic decision-making.";
         document.querySelector("#projects .project:nth-child(3) a").innerText = "View Project";
+        document.querySelector("#testimonials h2").innerText = "Testimonials";
+        document.querySelector("#testimonials .testimonial:nth-child(1) p:nth-child(2)").innerText = "Jesus is an amazing developer. His work has been essential to the success of our project.";
+        document.querySelector("#testimonials .testimonial:nth-child(1) p:nth-child(3)").innerText = "- Maria Gonzalez, CEO of TechCorp";
+        document.querySelector("#testimonials .testimonial:nth-child(2) p:nth-child(2)").innerText = "The quality and attention to detail that Jesus offers is exceptional. I would recommend him without hesitation.";
+        document.querySelector("#testimonials .testimonial:nth-child(2) p:nth-child(3)").innerText = "- Juan Perez, Project Manager at Innovate Ltd.";
         document.querySelector("#contact h2").innerText = "Contact";
-        document.querySelector("#contact p:nth-child(2)").innerText = "Email: jesuscarrilero@example.com";
-        document.querySelector("#contact p:nth-child(3)").innerText = "Phone: +34 616 460 352";
+        document.querySelector("#contact form label[for='name']").innerText = "Name:";
+        document.querySelector("#contact form label[for='email']").innerText = "Email:";
+        document.querySelector("#contact form label[for='message']").innerText = "Message:";
+        document.querySelector("#contact form button").innerText = "Send";
     }
 }
+
 const carouselSlide = document.querySelector('.carousel-slide');
 const images = document.querySelectorAll('.carousel-slide img');
 
